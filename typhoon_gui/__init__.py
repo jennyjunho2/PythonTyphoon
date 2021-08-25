b343e3fde173530444d99f7ef1f5539721d573d1
jennyjunho2 <a01032208149@gmail.com>
2021-08-25 17:32:06 +0900

    - UI 제작중...

--- typhoon_gui/__init__.py
+++ typhoon_gui/__init__.py
@@ -1,23 +1,29 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5 import QtGui
+from PyQt5.QtGui import QIcon
 
-form_class = uic.loadUiType("test.ui")[0]
 
-class WindowClass(QDialog, form_class) :
+class WindowClass(QDialog) :
     def __init__(self) :
         super().__init__()
-        self.setupUi(self)
-        self.setFixedSize(1151, 481)
+        self.ui = uic.loadUi("test.ui", self)
+        self.setWindowTitle("검토용파일 제작 프로그램")
+        self.setWindowIcon(QIcon("icon.png"))
+        self.pushButton_execute.clicked.connect(self.execute_function)
 
-class first_tab(QWidget):
-    def __init__(self):
-        super().__init__()
-
-
-    def getText(self):
+    def getText_excel_directory(self):
         excel_directory = self.QTextEdit_excel_directory.toPlainText()
+
+    def getText_test_name(self):
+        test_name = self.QTextEdit_test_name.toPlainText()
+
+    def execute_function(self):
+        excel_directory = self.QTextEdit_excel_directory.toPlainText()
+        test_name = self.QTextEdit_test_name.toPlainText()
         print(excel_directory)
+        print(test_name)
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
