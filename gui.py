f7cf7fa8b32c9fec144a5b76319fa263a4f0a784
이준호 <a01032208149@gmail.com>
2021-10-04 15:59:17 +0900

    - minor bug fixed

--- gui.py
+++ gui.py
@@ -321,11 +321,13 @@ def basefile_to_source_gui(hwp, basefile : str, test_name : str, grade_number, i
     field_list_change_solution_number = list(map(lambda y : int(y)-1, list(map(lambda x: x[:-5], field_list_change_solution_number))))
     problem_change_problem = problems.iloc[field_list_change_problem_number]
     problem_change_solution = problems.iloc[field_list_change_solution_number]
-    for i in range(problem_change_problem.shape[0]):
+    for i in range(problem_change_problem.shape[0]): # 각 문제에 대하여
         problem_change_problem_set = problem_change_problem.iloc[i]
         src = array_to_problem_directory(problem_change_problem_set, grade=grade_number, test_name = test_name, for_release = False, ip_address = ip_address, test = test_mode)
         problem_directory, src_problem_number, dst_problem_number, src_problem_score = src[0], src[1], src[2], src[3]
         myWindow.appendTextFunction_2(string = f"{field_list_change_problem_number[i]+1}번문제 반영중...({i+1}번째 입력)")
+
+        # 반영 전 검토용파일 문제 복사하기
         hwp.Open(rf'{basefile}')
         shape_copy_paste(hwp)
         hwp.MoveToField(f"{field_list_change_problem_number[i]+1}번문제글상자", start = True)
@@ -340,6 +342,7 @@ def basefile_to_source_gui(hwp, basefile : str, test_name : str, grade_number, i
         hwp.HAction.Run("Copy")
         hwp.HAction.Run("MoveDown")
 
+        # 문제저장용에 붙여넣기
         if os.path.exists(problem_directory) == False:
             raise Exception(f"{field_list_change_problem_number[i]+1}번문제 문제저장용 파일이 존재하지 않습니다!")
         hwp.Open(rf"{problem_directory}")
@@ -356,6 +359,10 @@ def basefile_to_source_gui(hwp, basefile : str, test_name : str, grade_number, i
         hwp.Run("DeleteBack")
         hwp.SetPos(*start_pos)
         hwp.Run("Paste")
+        hwp.MoveToField(f"{src[1]}번문제")
+        hwp.HAction.Run("MoveRight")
+        hwp.HAction.Run("StyleShortcut2")
+
         hwp.Save()
         myWindow.appendTextFunction_2(string = f"{field_list_change_problem_number[i] + 1}번문제 반영완료! ({i + 1}번째 입력)")
         progress += (100 // (problem_change_problem.shape[0]+problem_change_solution.shape[0]))
